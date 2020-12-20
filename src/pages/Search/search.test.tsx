55d58489df44ed4aa0c8539847a210b945c60e07
yuri014 <yuriflores2012@gmail.com>
2020-12-20 12:15:41 -0300

    simple test

--- src/pages/Search/search.test.tsx
+++ src/pages/Search/search.test.tsx
@@ -1,1 +1,11 @@
-test('renders without error', () => {});
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import SearchPage from '.';
+import findByTestAttribute from '../../tests/utils';
+
+test('renders without error', () => {
+  const wrapper = shallow(<SearchPage />);
+  const searchPageComponent = findByTestAttribute(wrapper, 'search-page');
+  expect(searchPageComponent.length).toBe(1);
+});
