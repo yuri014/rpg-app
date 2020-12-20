f328dba9b7ff13779a9a8578d306a1c041e4bb59
yuri014 <yuriflores2012@gmail.com>
2020-12-20 14:09:19 -0300

    test if the footer guides have three links

--- src/routes/FooterTabs.test.tsx
+++ src/routes/FooterTabs.test.tsx
@@ -4,8 +4,15 @@ import { shallow } from 'enzyme';
 import FooterTabs from './FooterTabs';
 import findByTestAttribute from '../tests/utils';
 
-test('renders withou error', () => {
-  const wrapper = shallow(<FooterTabs />);
-  const footerTabsComponents = findByTestAttribute(wrapper, 'footer-tabs');
-  expect(footerTabsComponents.length).toBe(1);
+describe('renders without error', () => {
+  test('renders component without error', () => {
+    const wrapper = shallow(<FooterTabs />);
+    const footerTabsComponents = findByTestAttribute(wrapper, 'footer-tabs');
+    expect(footerTabsComponents.length).toBe(1);
+  });
+  test('renders three screens without error', () => {
+    const wrapper = shallow(<FooterTabs />);
+    const screens = findByTestAttribute(wrapper, 'icon-image');
+    expect(screens.length).toBe(3);
+  });
 });
