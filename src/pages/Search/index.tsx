45314f38eafd6ef2fe8575b34cb741ea9ed03ccf
yuri014 <yuriflores2012@gmail.com>
2020-12-20 03:06:31 -0300

    adding search page

---
+++ src/pages/Search/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function SearchPage() {
+  return (
+    <View>
+      <Text>TEST</Text>
+    </View>
+  );
+}
+export default SearchPage;
