177e6ae4b0c4d484b3ace54a8a1142d9fc3eb373
yuri014 <yuriflores2012@gmail.com>
2020-12-20 12:15:27 -0300

    adding new colors

--- src/theme/global.ts
+++ src/theme/global.ts
@@ -2,6 +2,8 @@ const theme = {
   primaryColor: '#82C43B',
   secondaryColor: '#243678',
   backgroundColor: '#4F69C5',
+  lightBackgroundColor: '#BFCDFF',
+  textPrimaryColor: '#22282B',
 };
 
 export default theme;
