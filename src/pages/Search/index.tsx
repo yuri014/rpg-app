f8e9b8efcfadb481b1b0e8ceaa55c8d5a2567b81
yuri014 <yuriflores2012@gmail.com>
2020-12-20 12:20:49 -0300

    finish search page layout

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, Text, View } from 'react-native';
+import { SearchBar } from 'react-native-elements';
+
+import robe from '../../../assets/icons/robe.png';
+import styles from './styles';
 
 function SearchPage() {
+  const [search, setSearch] = useState('');
+
   return (
-    <View>
-      <Text>TEST</Text>
+    <View style={styles.container} data-test="search-page">
+      <View>
+        <SearchBar
+          placeholder="Search..."
+          value={search}
+          onChangeText={(text) => setSearch(text)}
+          containerStyle={[styles.searchContainer, styles.shadow]}
+          inputContainerStyle={styles.searchBar}
+          style={styles.searchInput}
+        />
+      </View>
+      <View style={styles.mainContainer}>
+        <Image source={robe} />
+        <Text style={styles.initialText}>Use this spell to search DnD resources</Text>
+      </View>
     </View>
   );
 }
