ad745332dfed8e4034d1e71a06db0d500fa8d394
yuri014 <yuriflores2012@gmail.com>
2020-12-20 03:06:15 -0300

    changing way to navigate

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,22 +1,20 @@
-import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { Image, Text, View } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 
 import diceIcon from '../../../assets/icons/dice-twenty-faces-twenty.png';
+import { Navigation } from '../../interface/navigations';
 
 import styles from './styles';
 
-function Landing() {
-  const { navigate } = useNavigation();
-
+function Landing({ navigation }: Navigation) {
   return (
-    <View style={styles.pageContainer}>
+    <View data-test="landing-page" style={styles.pageContainer}>
       <View>
         <Text style={styles.title}>RPG-APP</Text>
         <Text style={styles.subtitle}>Simple way to search and save your DnD resources</Text>
       </View>
-      <RectButton style={styles.button} onPress={() => navigate('Search Page')}>
+      <RectButton style={styles.button} onPress={() => navigation?.navigate('Search Page')}>
         <Text style={styles.start}>Start</Text>
         <Image source={diceIcon} />
       </RectButton>
