2d43ab4bfca93775a1846a9ae6028193ff383641
yuri014 <yuriflores2012@gmail.com>
2020-12-20 15:18:45 -0300

    adding signika font

--- App.tsx
+++ App.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import { useFonts, Metamorphous_400Regular } from '@expo-google-fonts/metamorphous';
+import { Signika_300Light, Signika_400Regular } from '@expo-google-fonts/signika';
+
 import AppStack from './src/routes/App';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Metamorphous_400Regular,
+    Signika_400Regular,
+    Signika_300Light,
   });
 
   if (!fontsLoaded) {
