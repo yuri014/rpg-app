f60b7cc7111558d46a51151f655fa9841f2375d6
yuri014 <yuriflores2012@gmail.com>
2020-12-20 03:06:51 -0300

    adding search page and typing stack

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Landing from '../pages/Landing';
 
-const { Navigator, Screen } = createStackNavigator();
+import Landing from '../pages/Landing';
+import SearchPage from '../pages/Search';
+import { RootStackParamList } from '../interface/navigations';
+
+const { Navigator, Screen } = createStackNavigator<RootStackParamList>();
 
 function AppStack() {
   return (
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="Landing" component={Landing} />
+        <Screen name="Search Page" component={SearchPage} />
       </Navigator>
     </NavigationContainer>
   );
