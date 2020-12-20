6766446c031ba95fa428f428e940a4eb527f4d69
yuri014 <yuriflores2012@gmail.com>
2020-12-20 15:19:54 -0300

    home page styles

---
+++ src/pages/Home/style.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+import theme from '../../theme/global';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.backgroundColor,
+    flex: 1,
+    alignItems: 'center',
+    paddingHorizontal: 24,
+    paddingVertical: 72,
+  },
+});
+
+export default styles;
