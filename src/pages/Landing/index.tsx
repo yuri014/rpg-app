53a6499b7e34096988a4e26d8d683d9be93949ba
yuri014 <yuriflores2012@gmail.com>
2020-12-20 01:27:42 -0300

    adding link

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,21 +1,25 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { Image, Text, View } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import diceIcon from '../../../assets/icons/dice-twenty-faces-twenty.png';
 
 import styles from './styles';
 
 function Landing() {
+  const { navigate } = useNavigation();
+
   return (
     <View style={styles.pageContainer}>
       <View>
         <Text style={styles.title}>RPG-APP</Text>
         <Text style={styles.subtitle}>Simple way to search and save your DnD resources</Text>
       </View>
-      <View>
+      <RectButton style={styles.button} onPress={() => navigate('Search Page')}>
         <Text style={styles.start}>Start</Text>
         <Image source={diceIcon} />
-      </View>
+      </RectButton>
     </View>
   );
 }
