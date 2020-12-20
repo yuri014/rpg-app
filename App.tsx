7452b3c2a9e9dcc1100ec38458d4c8a691748388
yuri014 <yuriflores2012@gmail.com>
2020-12-20 00:24:08 -0300

    hello world component

--- App.tsx
+++ App.tsx
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+/* eslint-disable camelcase */
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
+import { useFonts, Metamorphous_400Regular } from '@expo-google-fonts/metamorphous';
+import AppStack from './src/routes/App';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Metamorphous_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
+      <AppStack />
+      {/* eslint-disable-next-line react/style-prop-object */}
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
