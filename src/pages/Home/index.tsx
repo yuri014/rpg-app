b89adb5c070351c4c51f4754c0befaf82400596b
yuri014 <yuriflores2012@gmail.com>
2020-12-21 18:50:33 -0300

    adding spell card

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
-import { View } from 'react-native';
+import { SafeAreaView, ScrollView, View } from 'react-native';
 
-import SpellHomeCard from '../../components/HomeCards/SpellHomeCard';
+import HomeCards from '../../components/HomeCards';
 import styles from './style';
+import spellTitleIcon from '../../../assets/icons/magic-swirl.png';
+import spellBookIcon from '../../../assets/icons/spell-book.png';
+import SpellHomeCard from '../../components/HomeCards/SpellHomeCard';
 
 function Home() {
   return (
-    <View style={styles.container} data-test="home-page">
-      <SpellHomeCard />
-    </View>
+    <SafeAreaView data-test="home-page">
+      <ScrollView>
+        <View style={styles.container}>
+          <HomeCards
+            titleImage={spellTitleIcon}
+            cardImage={spellBookIcon}
+            header={{
+              title: 'Detect Magic',
+              subtitle: ['Lv: 1', 'Range: Self', 'School: Divination'],
+            }}
+            title="Day's Spells"
+            data-test="home-card-component"
+          >
+            <SpellHomeCard classes="Bard, Cleric, Druid, Paladin, Ranger, Sorcerer, Wizard">
+              For the duration, you sense the presence of magic within 30 feet of you. If you sense
+              magic in this way, you can use your action to see a faint aura around any visible
+              creature or object in the area that bears magic, and you learn its school of magic, if
+              any. The spell can penetrate most barriers, but it is blocked by 1 foot of stone, 1
+              inch of common metal, a thin sheet of lead, or 3 feet of wood or dirt.
+            </SpellHomeCard>
+          </HomeCards>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
