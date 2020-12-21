33850fc2885c9ee0a0932786a314a2661ee17ff8
yuri014 <yuriflores2012@gmail.com>
2020-12-21 18:51:06 -0300

    card container to other home page cards

---
+++ src/components/HomeCards/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  Image, ImageSourcePropType, Text, View,
+} from 'react-native';
+
+import { cardStyleContainer } from './styles';
+
+interface CardsProps {
+  title: string;
+  titleImage: ImageSourcePropType;
+  cardImage: ImageSourcePropType;
+  children: React.ReactNode;
+  header: {
+    title: string;
+    subtitle: string[];
+  };
+}
+
+const HomeCards = ({
+  titleImage, cardImage, children, header, title,
+}: CardsProps) => (
+  <View>
+    <View style={cardStyleContainer.mainTitleContainer}>
+      <Text style={cardStyleContainer.mainTitle}>{title}</Text>
+      <Image style={cardStyleContainer.iconTitle} source={titleImage} />
+    </View>
+    <View style={cardStyleContainer.container}>
+      <View style={cardStyleContainer.cardHeader}>
+        <Image style={{ width: 48, height: 48 }} source={cardImage} />
+        <View style={cardStyleContainer.headerText}>
+          <Text style={cardStyleContainer.title}>{header.title}</Text>
+          <View style={cardStyleContainer.cardHeader}>
+            {header.subtitle.map((item) => (
+              <Text key={item} style={cardStyleContainer.subtitle}>
+                {item}
+                &nbsp;
+              </Text>
+            ))}
+          </View>
+        </View>
+      </View>
+      <View>{children}</View>
+    </View>
+  </View>
+);
+
+export default HomeCards;
