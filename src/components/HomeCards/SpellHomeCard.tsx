29e0f7609d88a970b8baaf4dcf189aac90ce75b8
yuri014 <yuriflores2012@gmail.com>
2020-12-21 18:50:53 -0300

    spell home card

--- src/components/HomeCards/SpellHomeCard.tsx
+++ src/components/HomeCards/SpellHomeCard.tsx
@@ -1,35 +1,17 @@
 import React from 'react';
-import { Image, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
-import spellBookIcon from '../../../assets/icons/spell-book.png';
-import { cardStyleContainer, spellStyleCard } from './styles';
+import { spellStyleCard } from './styles';
 
-const SpellHomeCard = () => (
+const SpellHomeCard = ({ classes, children }: { classes: string; children: React.ReactNode }) => (
   <View>
-    <View>
-      <Text>Day&apos;s Spells</Text>
-    </View>
-    <View style={cardStyleContainer.container}>
-      <View style={cardStyleContainer.cardHeader}>
-        <Image style={{ width: 50, height: 50 }} source={spellBookIcon} />
-        <View style={cardStyleContainer.headerText}>
-          <Text style={cardStyleContainer.title}>Detect Magic</Text>
-          <View style={cardStyleContainer.cardHeader}>
-            <Text style={cardStyleContainer.subtitle}>LV: 1 &nbsp;</Text>
-            <Text style={cardStyleContainer.subtitle}>Range: Self &nbsp;</Text>
-            <Text style={cardStyleContainer.subtitle}>School: Divination &nbsp;</Text>
-          </View>
-        </View>
-      </View>
-      <View>
-        <Text style={spellStyleCard.description}>
-          For the duration, you sense the presence of magic within 30 feet of you. If you sense
-          magic in this way, you can use your action to see a faint aura around any visible creature
-          or object in the area that bears magic, and you learn its school of magic, if any. The
-          spell can penetrate most barriers, but it is blocked by 1 foot of stone, 1 inch of common
-          metal, a thin sheet of lead, or 3 feet of wood or dirt.
-        </Text>
-      </View>
+    <Text style={spellStyleCard.description}>{children}</Text>
+    <View style={spellStyleCard.classes}>
+      <Text style={spellStyleCard.classTitle}>
+        Classes:
+        &nbsp;
+        <Text style={spellStyleCard.classContent}>{classes}</Text>
+      </Text>
     </View>
   </View>
 );
