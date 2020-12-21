6369f7b90bbf3ac7512467af311d53f70165bc33
yuri014 <yuriflores2012@gmail.com>
2020-12-21 18:49:33 -0300

    test home page content

--- src/pages/Home/home.test.tsx
+++ src/pages/Home/home.test.tsx
@@ -4,8 +4,16 @@ import { shallow } from 'enzyme';
 import Home from './index';
 import findByTestAttribute from '../../tests/utils';
 
-test('renders without error', () => {
-  const wrapper = shallow(<Home />);
-  const homePage = findByTestAttribute(wrapper, 'home-page');
-  expect(homePage.length).toBe(1);
+describe('renders the page correctly', () => {
+  test('renders without error', () => {
+    const wrapper = shallow(<Home />);
+    const homePage = findByTestAttribute(wrapper, 'home-page');
+    expect(homePage.length).toBe(1);
+  });
+
+  test('renders four cards', () => {
+    const wrapper = shallow(<Home />);
+    const homeCardPage = findByTestAttribute(wrapper, 'home-card-component');
+    expect(homeCardPage.length).toBe(4);
+  });
 });
