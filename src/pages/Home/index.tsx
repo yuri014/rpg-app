ed88578bf787bc92b612828503e9f958afd8444c
yuri014 <yuriflores2012@gmail.com>
2020-12-21 22:05:56 -0300

    adding monster card to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { SafeAreaView, ScrollView, View } from 'react-native';
 
-import HomeCards from '../../components/HomeCards';
 import styles from './style';
+import HomeCards from '../../components/HomeCards';
+
 import spellTitleIcon from '../../../assets/icons/magic-swirl.png';
 import spellBookIcon from '../../../assets/icons/spell-book.png';
 import SpellHomeCard from '../../components/HomeCards/SpellHomeCard';
 
+import monsterTitleIcon from '../../../assets/icons/wyvern.png';
+import monsterCardIcon from '../../../assets/icons/werewolf.png';
+import MonsterCard from '../../components/HomeCards/MonsterCard';
+
+import shieldIcon from '../../../assets/icons/round-shield.png';
+import hpIcon from '../../../assets/icons/life-bar.png';
+import diceIcon from '../../../assets/icons/dice-card.png';
+
 function Home() {
   return (
     <SafeAreaView data-test="home-page">
-      <ScrollView>
+      <ScrollView style={styles.backgroundPage}>
         <View style={styles.container}>
           <HomeCards
             titleImage={spellTitleIcon}
@@ -29,6 +38,24 @@ function Home() {
               any. The spell can penetrate most barriers, but it is blocked by 1 foot of stone, 1
               inch of common metal, a thin sheet of lead, or 3 feet of wood or dirt.
             </SpellHomeCard>
+          </HomeCards>
+
+          <HomeCards
+            titleImage={monsterTitleIcon}
+            cardImage={monsterCardIcon}
+            header={{
+              title: 'Griffon',
+              subtitle: ['Size: Large', 'Type: Monstrosity'],
+            }}
+            title="Day's Monster"
+            data-test="home-card-component"
+          >
+            <MonsterCard
+              shieldIcon={shieldIcon}
+              diceIcon={diceIcon}
+              hpIcon={hpIcon}
+              actions="Multiattack, Beak, Claws"
+            />
           </HomeCards>
         </View>
       </ScrollView>
