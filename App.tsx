a565bbddb98f1e5c8f416251f48d57a562ab2b6f
yuri014 <yuriflores2012@gmail.com>
2020-12-22 23:46:11 -0300

    adding apollo provider

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 /* eslint-disable camelcase */
 import React from 'react';
+import { AppRegistry } from 'react-native';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import { useFonts, Metamorphous_400Regular } from '@expo-google-fonts/metamorphous';
 import { Signika_300Light, Signika_400Regular } from '@expo-google-fonts/signika';
 
 import AppStack from './src/routes/App';
+
+const client = new ApolloClient({
+  uri: 'https://www.dnd5eapi.co/graphql',
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,9 +27,13 @@ export default function App() {
 
   return (
     <>
-      <AppStack />
+      <ApolloProvider client={client}>
+        <AppStack />
+      </ApolloProvider>
       {/* eslint-disable-next-line react/style-prop-object */}
       <StatusBar style="auto" />
     </>
   );
 }
+
+AppRegistry.registerComponent('MyApplication', () => App);
