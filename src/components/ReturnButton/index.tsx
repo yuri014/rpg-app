ab3f9e3af8c4000238e9804b58d90edd9877e483
yuri014 <yuriflores2012@gmail.com>
2020-12-23 17:44:15 -0300

    adding goBack to return button

--- src/components/ReturnButton/index.tsx
+++ src/components/ReturnButton/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { Image, View } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
@@ -5,12 +6,15 @@ import { RectButton } from 'react-native-gesture-handler';
 import returnIcon from '../../../assets/icons/broadhead-arrow.png';
 import styles from './style';
 
-const ReturnButton = () => (
-  <View style={styles.returnButtonContainer}>
-    <RectButton style={styles.returnButton}>
-      <Image style={styles.returnIcon} source={returnIcon} />
-    </RectButton>
-  </View>
-);
+const ReturnButton = () => {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.returnButtonContainer}>
+      <RectButton onPress={() => navigation?.goBack()} style={styles.returnButton}>
+        <Image style={styles.returnIcon} source={returnIcon} />
+      </RectButton>
+    </View>
+  );
+};
 
 export default ReturnButton;
