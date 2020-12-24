92c8d22b5598a1fe3b1e70d25c3bc3a9e86c6e99
yuri014 <yuriflores2012@gmail.com>
2020-12-24 02:08:46 -0300

    adding reducer to filter and update component in future

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,13 +1,44 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
 import { Image, Text, View } from 'react-native';
-import { SearchBar } from 'react-native-elements';
+import { ButtonGroup, SearchBar } from 'react-native-elements';
 
 import robe from '../../../assets/icons/robe.png';
 import ReturnButton from '../../components/ReturnButton';
 import styles from './styles';
 
+const initialState = {
+  node: (
+    <>
+      <Image source={robe} />
+      <Text style={styles.initialText}>Use the spell below to filter DnD resources</Text>
+    </>
+  ),
+};
+
+function reducer(state: { node: React.ReactNode }, action: { type: number; payload: string }) {
+  switch (action.type) {
+    case 0:
+      return { node: <Text>spell</Text> };
+    case 1:
+      return { node: <Text>equip</Text> };
+    case 2:
+      return { node: <Text>monster</Text> };
+    default:
+      return state;
+  }
+}
+
 function SearchPage() {
   const [search, setSearch] = useState('');
+  const [selectedButton, setSelectedButton] = useState(10);
+
+  const spellButton = () => <Text>Spells</Text>;
+  const equipButton = () => <Text>Equipment</Text>;
+  const monstersButton = () => <Text>Monsters</Text>;
+
+  const buttons = [{ element: spellButton }, { element: equipButton }, { element: monstersButton }];
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <>
@@ -21,11 +52,20 @@ function SearchPage() {
             containerStyle={[styles.searchContainer, styles.shadow]}
             inputContainerStyle={styles.searchBar}
             style={styles.searchInput}
+            onSubmitEditing={() => dispatch({ type: selectedButton, payload: search })}
           />
         </View>
-        <View style={styles.mainContainer}>
-          <Image source={robe} />
-          <Text style={styles.initialText}>Use this spell to search DnD resources</Text>
+        <View style={styles.mainContainer}>{state && state.node}</View>
+        <View>
+          <ButtonGroup
+            onPress={(e) => setSelectedButton(e)}
+            selectedIndex={selectedButton}
+            buttons={buttons}
+            containerStyle={styles.buttonGroupContainer}
+            buttonStyle={styles.buttonGroup}
+            selectedButtonStyle={styles.buttonGroupActive}
+            textStyle={styles.buttonGroupText}
+          />
         </View>
       </View>
     </>
