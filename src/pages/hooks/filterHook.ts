0eb5b888e46ccea0a4b4d294192329420e189b8b
yuri014 <yuriflores2012@gmail.com>
2020-12-25 18:24:26 -0300

    deleted filterHook

--- src/pages/hooks/filterHook.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable implicit-arrow-linebreak */
-const useFilter = (data: [], search: string) =>
-  data.filter((item: { index: string }) => item.index.includes(search));
-
-export default useFilter;
